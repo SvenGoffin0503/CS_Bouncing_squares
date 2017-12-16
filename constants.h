80fd71b68b5bddbab0be38ddb7955d00ab265e85
Sven Goffin <Sven.Goffin@student.ulg.ac.be>
2017-12-16 01:27:00 +0100

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,10 @@
+/* ==========================================================================
+ constants.h : constants definition file
+ ============================================================================ */
+
+#define SIZE_X 256
+#define SIZE_Y 256
+#define PIXEL_WIDTH 2
+#define SQUARE_WIDTH 16
+#define SQUARE_COUNT 4
+#define SQUARE_MAX 10
